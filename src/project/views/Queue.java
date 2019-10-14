02298b2e18fd2457c5f753a56b2aae6d6bc737aa
Paul <paul.retail@me.com>
2019-10-14 14:49:40 +0200

    second commit

--- src/project/views/Queue.java
+++ src/project/views/Queue.java
@@ -4,4 +4,11 @@ import javax.swing.*;
 
 public class Queue {
     public JPanel jpanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton calculerButton;
 }
